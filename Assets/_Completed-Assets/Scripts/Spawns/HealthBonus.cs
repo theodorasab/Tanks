b9fe8a958927671d69c8e341a48b360d10925b3e
Theodora Sampalou <theosaba2000@gmail.com>
2020-12-09 15:49:51 +0200

    uploading tanks project

---
+++ Assets/_Completed-Assets/Scripts/Spawns/HealthBonus.cs
@@ -0,0 +1,33 @@
+﻿//!
+using System;
+using UnityEngine;
+
+[Serializable]
+public class HealthBonus
+{
+
+	[HideInInspector] public GameObject m_Instance;
+	[HideInInspector] public int timeout;
+	[HideInInspector] public float timestamp;
+	[HideInInspector] public bool isTrap = false;
+	[HideInInspector] public bool isStun = false;
+	private HealthBonusMono mono;
+
+	public void Setup() {
+		mono = m_Instance.GetComponent<HealthBonusMono> () as HealthBonusMono;
+		if (isTrap)
+		{
+			m_Instance.name = "Trap";
+		}
+		else if (isStun)
+		{
+			m_Instance.name = "Stun";
+		}
+		else
+		{
+			m_Instance.name = "Health";
+		}
+	} //ctor
+
+} //HealthBonus
+//^
---
+++ Assets/_Completed-Assets/Scripts/Spawns/HealthBonusMono.cs
@@ -0,0 +1,57 @@
+﻿//!
+using UnityEngine;
+
+public class HealthBonusMono : MonoBehaviour
+{
+	bool moves = false;
+	private Rigidbody body;
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		body = gameObject.GetComponent<Rigidbody> ();
+
+		int move = Random.Range(0, 5);
+
+		if (move == 0)
+		{
+			moves = true;
+		}
+
+		Debug.Log("Spawned" + (moves? " movable " : " ") + "box");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (moves)  //TODO: IMPL wall-collision
+		{
+			Vector3 by = new Vector3(Random.Range(0, 50) * 0.001f, 0f, Random.Range(0, 50) * 0.001f);
+			body.MovePosition(transform.position + by);
+		} else if (name == "Stun")
+		{
+			Vector3 by = new Vector3(Random.Range(0, 50) * 0.003f, 0f, Random.Range(0, 50) * 0.003f);
+			body.MovePosition(transform.position + by);
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.name == "Tank 0" || other.gameObject.name == "Tank 1")  //All tanks prefabs spawn with that name. If box collides with tank:
+		{
+			TankHealth hp = other.gameObject.GetComponent<TankHealth>();  //Target collided tank health module
+			if (gameObject.name == "Trap")  //in case consumable is a Trap
+			{
+				hp.TakeDamage();
+				gameObject.SetActive(false);
+			}
+			else if (gameObject.name == "Health")  //in case consumable is a HealthBox
+			{
+				hp.Heal();
+				gameObject.SetActive(false);
+			}  //in case consumable is a Stun, pass handling on TankMovement passive script
+		}  //box collides with wall:
+	}
+}
+//^
